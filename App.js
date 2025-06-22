ed461221fb8ecc2bf3beb631fff63009ed489ea9
Ankit Raj <ankitrajcollab@gmail.com>
2025-06-22 21:43:56 +0530

    feat : add attributes in h1

---
+++ App.js
@@ -0,0 +1,6 @@
+// here we will write our react code
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+

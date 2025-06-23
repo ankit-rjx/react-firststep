9a6abdc4e0838add6d1bff6161f8d5080cc49d84
Ankit Raj <ankitrajcollab@gmail.com>
2025-06-23 09:42:18 +0530

    feat : add nested div react code

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 // here we will write our react code
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const parent = React.createElement(
+  // parent is a object and just a react element
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" }, "i am a h1 tag in nested div")
+  )
+);
 
+// this is a object - we are creating a react element at the end
+const heading = React.createElement(
+  "h1",
+  { id: "heading" }, // attributes
+  "Hello World from React!" // this is the children it will go inside h1 tag .
+);
+
+// console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+// React Element is nothing but a normal js object it has something known as props - props are children and the the attributes that we have passed to the react element .
